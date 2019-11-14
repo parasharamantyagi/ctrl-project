59307c5c4c15a5d57b0d3a623b8441e9fa26a30f
Ubuntu <ubuntu@ip-172-31-95-104.ec2.internal>
2019-11-14 16:55:58 +0000

    login change meta contant

--- resources/views/admin/Login/login.blade.php
+++ resources/views/admin/Login/login.blade.php
@@ -4,16 +4,9 @@
 <head>
 <meta charset="utf-8">
 <link rel="shortcut icon" href="http://18.212.23.117/blog/public/assets/images/icone.jpg">
-<title>Punjabi Books | eBookBazaar</title>
+<title>CTRL</title>
 		
-		<meta name="description" content="eBookBazaar is an online platform to buy Punjabi Books,Religious Books,Literature,Monographs,Novels,Poetry. Both Physical as well Digitized form.">
-		<meta name="keywords" content="Punjabi books buy online,Punjabi Books,Punjabi Poetry,Sikh Religious Books,Punjabi Novels,Punjabi Literature,Buy Punjabi Books Online,Punjabi History,punjabi literature sites.">
-			
-		<!-- Mobile Specific Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<!-- Store CSRF token for AJAX calls -->
-			
-		<meta name="csrf-token" content="zxogo7sP1DEjcWWYxqVI3FzrDoN4UfH9codfC5xY">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<!-- Stylesheets -->
 		<link href="{{ url('/public/assets/bootstrap/bootstrap.min.css') }}" rel="stylesheet"><!-- Bootstrap core CSS -->
 		<link href="https://ebookbazaar.com/style/font-awesome.min.css" rel="stylesheet"><!-- font-awesome Bootstrap core CSS -->
@@ -27,14 +20,7 @@
 </head>
 		<body>
 			<div class="top_header">
-			<div class="social_media">
-				<ul>
-					<li><a href="https://www.facebook.com/eBookBazaar/"><i class="fa fa-facebook-square"></i></a></li>
-					<li><a href="https://twitter.com/ebookbazaar"><i class="fa fa-twitter-square"></i></a></li>
-					<li><a href="https://plus.google.com/u/0/107545404788310748556"><i class="fa fa-google-plus-square"></i></a></li>
-					<li><a href="https://in.pinterest.com/ebookbazaar"><i class="fa fa-pinterest-square"></i></a></li>
-				</ul>
-			</div>
+		
 		</div><!--/top_header-->
 <style>
 .top_header, .header, .user_breadcrumbs {display:none !important;}
@@ -44,7 +30,7 @@
 <div class="login_details_admin_outer">			
 <div class="login_details">			
 <div class="logo">
-			<a href="https://ebookbazaar.com"><img class="login-image-icone" alt="eBookBazaar" src="{{ url('public/ctrl-icon/ctrl_title3x.png') }}"></a>
+			<a href="{{ url('/admin') }}"><img class="login-image-icone" alt="eBookBazaar" src="{{ url('public/ctrl-icon/ctrl_title3x.png') }}"></a>
 		</div>			
 <div class="login_innerdetails">
                     <form class="form-horizontal" role="form" method="POST" action="">
